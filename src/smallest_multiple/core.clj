2b7c1897caba94b2952108065c1db7d8c4f43f80
Jonathan <jonathan.e.corrales@gmail.com>
2017-08-06 16:42:49 -0400

    Redid the entire solution to use a more mathematical approach. Updated unit tests as well.

--- src/smallest_multiple/core.clj
+++ src/smallest_multiple/core.clj
@@ -1,29 +1,61 @@
 (ns smallest-multiple.core
   (:gen-class))
 
-  (defn smallest-multiple? [number max]
-   "Checks if number is evenly divisible by all numbers from 1 to max"
-    (if (not (zero? (mod number max)))
-      false
-      (let [mods (map #(mod number %) (range (dec max) 0 -1))
-            zeros (take-while zero? mods)]
-       (if (not= (count zeros) (dec max)) ;; skipping max
-         false
-         true
-       ))))
+(defn prime? [number]
+  (if (<= number 1)
+   false
+  (if (= number 2)
+   true
+  (= nil (first
+           (for [i (range 2 number)
+            :when (zero? (mod number i))] i))))))
 
-  (defn get-middle-value [number]
-    (inc (int (/ number 2))))
+(defn get-smallest-prime-factor [number]
+  (first (filter
+      #(and (prime? %) (zero? (mod number %)))
+      (range 2 (inc number)))))
 
-  (defn find-smallest-multiple [max]
-      (let [ min (get-middle-value max)
-             largest-multiple (reduce * (range min (inc max)))
-             ;; equivalent to max!/(max/2)! which reduces the size of the range
-             composites (range min (inc largest-multiple))]
-        (first (for [composite composites
-              :when (smallest-multiple? composite max)]
-              composite))))
 
+
+(defn prime-factorize [number]
+  (loop [no number result []]
+        (if (prime? no)
+          (conj result no)
+          (do
+           (def prime-factor (get-smallest-prime-factor no))
+           (def quotient (quot no prime-factor))
+              (if (prime? quotient)
+                 (conj result prime-factor quotient)
+                 (recur quotient (conj result prime-factor)))
+              ))))
+
+(defn get-prime-factors-for-range [range]
+  (sort (map (partial prime-factorize) range)))
+
+(defn get-highest-exponents [coll-of-vectors]
+  (loop [vectors coll-of-vectors result {}]
+   (if (empty? vectors)
+      result
+      (do
+       (let [first-vector (first vectors)]
+          (recur (rest vectors)
+                 (assoc result (first first-vector)
+                               (count first-vector))))))))
+
+(defn x-to-the-y-power [x y]
+  (reduce * (repeat y x)))
+
+(defn compute-exponents [map]
+ (for [keyval map]
+      (x-to-the-y-power (key keyval) (val keyval))))
+
+(defn find-smallest-multiple [max]
+  (->> (range 2 (inc max))
+       (get-prime-factors-for-range)
+       (get-highest-exponents)
+       (compute-exponents)
+       (reduce *)
+       ))
 
 (defn -main
   [& args]
--- test/smallest_multiple/core_test.clj
+++ test/smallest_multiple/core_test.clj
@@ -2,18 +2,32 @@
   (:require [clojure.test :refer :all]
             [smallest-multiple.core :refer :all]))
 
-(deftest test-smallest-multiple?-2520-to10-true
- (testing "2520 is divisible by all numbers from 1 to 10"
-   (is true? (smallest-multiple? 2520 10))))
+  (deftest prime-factorize-2
+    (testing "the prime factor of 2 is 2"
+      (is (= (prime-factorize 2) [2]))))
 
-(deftest test-smallest-multiple?-2520-to20-false
- (testing "2520 is NOT divisible by all numbers from 1 to 20"
-   (is false? (smallest-multiple? 2520 20))))
+  (deftest prime-factorize-4
+    (testing "the prime factor of 4 is 2 2"
+      (is (= (prime-factorize 4) [2 2]))))
 
- (deftest find-smallest-multiple-from1to10-2520
-  (testing "2520 is the smallest-multiple divisible by 1 ... 10"
-     (is true? (find-smallest-multiple 10))))
+   (deftest prime-factorize-20
+     (testing "the prime factors of 20 are 2 2 5"
+      (is (= (prime-factorize 20) [2 2 5]))))
 
- 
+   (deftest prime-factorize-2520
+     (testing "the prime factors of 20 are 2 2 2 3 3 5 7"
+      (is (= (prime-factorize 2520) [2 2 2 3 3 5 7]))))
+
+    (deftest find-smallest-multiple-from1to10-2520
+     (testing "2520 is the smallest-multiple2 divisible by 1 ... 10"
+        (is (= (find-smallest-multiple 10) 2520))))
+
+    (deftest x-to-the-y-power-2-to-the-3
+      (testing "2 to the 3 power is 8"
+         (is (= (x-to-the-y-power 2 3) 8))))
+
+    (deftest compute-exponents-vector
+      (testing "map {2 3, 3 2, 5 1, 7 1} computes to [8, 9, 5, 7]"
+         (is (= (compute-exponents {2 3, 3 2, 5 1, 7 1}) [8, 9, 5, 7]))))
 
 ; (run-tests 'smallest-multiple.core-test)
